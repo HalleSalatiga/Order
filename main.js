1174085ca706ed2a4e05d65ef1a1b599da56dca9
Zidan Ardiansyah <panjihadi6@gmail.com>
2024-11-21 23:24:58 +0700

    "Updated cart functionality and styling in main.js and style.css"

--- main.js
+++ main.js
@@ -1,8 +1,31 @@
 //cart
-let cartIcon = document.querySelector('#cart-icon.bx.bx-shopping-bag');
+let cartIcon = document.querySelector('#cart-icon');
 let cart = document.querySelector('.cart');
 let closeCart = document.querySelector('.close-cart');
-
-cartIcon.onclick = () => {
+//Open Cart
+cartIcon.onClick = () => {
     cart.classList.add('active');
 };
+//Close Cart
+closeCart.onClick = () => {
+    cart.classList.remove('active');
+};
+
+
+//Cart Working JS
+if (document.readyState == 'loading'){
+    document.addEventListener('DOMContentLoaded', ready)
+}else{
+    ready();
+}
+
+//Making Function
+function ready(){
+    //Remove Item From Cart
+    var removeCartButtons = document.getElementsByClassName('cart-remove')
+    console.log(removeCartButtons)
+    for (var i = 0; i < removeCartButtons.length; i++){
+        var button = removeCartButtons[i]
+        button.addEventListener ('click', removeCartIte)
+    } 
+}
