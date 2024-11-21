8608ab51757ae320df594187ca67acea99270f7c
Zidan Ardiansyah <panjihadi6@gmail.com>
2024-11-21 22:22:37 +0700

    "Added script tag to index.html

---
+++ script.js
@@ -0,0 +1,8 @@
+//cart
+let cartIcon = document.querySelector('#cart-icon')
+let cart = document.querySelector('.cart')
+let closecart = document.querySelector('#close-cart')
+
+cartIcon,onclick=() =>{
+    cart.classList.add("active");
+}
