b59673a702929af947880dfae0246f9bee321ddb
Zidan Ardiansyah <panjihadi6@gmail.com>
2024-11-22 07:10:01 +0700

    "Refactored cart functionality and cleaned up CSS code"

--- main.js
+++ main.js
@@ -1,31 +1,31 @@
-// //cart
-// let cartIcon = document.querySelector('#cart-icon');
-// let cart = document.querySelector('.cart');
-// let closeCart = document.querySelector('bx bx-x');
-// //Open Cart
-// cartIcon.letcartIcon = () => {
-//     cart.classList.add('active');
-// };
-// //Close Cart
-// closeCart.letcartIcon = () => {
-//     cart.classList.remove('active');
-// };
+//cart
+let cartIcon = document.querySelector('#cart-icon');
+let cart = document.querySelector('.cart');
+let closeCart = document.querySelector('bx bx-x');
+//Open Cart
+cartIcon.onClick = () => {
+    cart.classList.add('active');
+};
+//Close Cart
+closeCart.onClick = () => {
+    cart.classList.remove('active');
+};
 
 
-// //Cart Working JS
-// if (document.readyState == 'loading'){
-//     document.addEventListener('DOMContentLoaded', ready)
-// }else{
-//     ready();
-// }
+//Cart Working JS
+if (document.readyState == 'loading'){
+    document.addEventListener('DOMContentLoaded', ready)
+}else{
+    ready();
+}
 
-// //Making Function
-// function ready(){
-//     //Remove Item From Cart
-//     var removeCartButtons = document.getElementsByClassName('cart-remove')
-//     console.log(removeCartButtons)
-//     for (var i = 0; i < removeCartButtons.length; i++){
-//         var button = removeCartButtons[i]
-//         button.addEventListener ('click', removeCartIte)
-//     } 
-// }
+//Making Function
+function ready(){
+    //Remove Item From Cart
+    var removeCartButtons = document.getElementsByClassName('cart-remove')
+    console.log(removeCartButtons)
+    for (var i = 0; i < removeCartButtons.length; i++){
+        var button = removeCartButtons[i]
+        button.addEventListener ('click', removeCartIte)
+    } 
+}
