8f30cada9d0dfd869a7e57eb061315125dcac006
Zidan Ardiansyah <panjihadi6@gmail.com>
2024-11-25 09:44:02 +0700

    update total function

--- main.js
+++ main.js
@@ -13,18 +13,56 @@ closeCart.addEventListener('click', () => {
 });
 
 //Cart Working JS
-if (document.readyState == 'loading'){
+if (document.readyState == 'loading') {
   document.addEventListener('DOMContentLoaded', ready);
 } else {
   ready();
 }
-//mMaking Function
-function ready(){
+
+//Making Function
+function ready() {
   //remove Items From Cart
-  var removeCartButtons  = document.getElementsByClassName('cart-remove')
-  console.log(removeCartButtons)
-  for (var i = 0; i < removeCartButtons.length; i++){
-    var button = removeCartButtons[i]
-    button.addEventListener('click', removeCartItems)
-    }
+  var removeCartButtons = document.getElementsByClassName('cart-remove');
+  console.log(removeCartButtons);
+  for (var i = 0; i < removeCartButtons.length; i++) {
+    var button = removeCartButtons[i];
+    button.addEventListener('click', removeCartItem); // Pass the function correctly
   }
+  //  Quantity Changes
+  var quantityInputs = document.getElementsByClassName('cart-quantity');
+  for (var i = 0; i < quantityInputs.length; i++) {
+    var input = quantityInputs[i]
+    input.addEventListener('change', quantityChanged);
+}
+}
+//Remove Items From Cart
+function removeCartItem(event) {
+  var buttonClicked = event.target;
+  buttonClicked.parentElement.remove();
+  updatetotal();
+}
+//Quantity Changes
+function quantityChanged(event) {
+  var input = event.target;
+  if (isNaN(input.value) || input.value <=0) {
+    input.value = 1;
+  }
+  updatetotal();
+}
+
+
+//Update Total
+function updatetotal() {
+  var cartContent = document.getElementsByClassName('cart-content')[0];
+  var cartBoxes = cartContent.getElementsByClassName('cart-box'); // Correct variable name
+  var total = 0;
+  for (var i = 0; i < cartBoxes.length; i++) {
+    var cartBox = cartBoxes[i]; // Correct variable name
+    var priceElement = cartBox.getElementsByClassName('cart-price')[0];
+    var quantityElement = cartBox.getElementsByClassName('cart-quantity')[0];
+    var price = parseFloat(priceElement.innerText.replace('$', ''));
+    var quantity = quantityElement.value;
+    total = total + price * quantity;
+  }
+  document.getElementsByClassName('total-price')[0].innerText = '$' + total;
+}
