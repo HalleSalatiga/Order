0d7a3858a543e755d0863c07ad652aba6a14ac5b
Zidan Ardiansyah <panjihadi6@gmail.com>
2024-11-25 08:27:22 +0700

    "Added event listener for cart remove buttons and DOMContentLoaded event handler"

--- main.js
+++ main.js
@@ -11,3 +11,20 @@ cartIcon.addEventListener('click', () => {
 closeCart.addEventListener('click', () => {
   cart.classList.remove('cart-active');
 });
+
+//Cart Working JS
+if (document.readyState == 'loading'){
+  document.addEventListener('DOMContentLoaded', ready);
+} else {
+  ready();
+}
+//mMaking Function
+function ready(){
+  //remove Items From Cart
+  var removeCartButtons  = document.getElementsByClassName('cart-remove')
+  console.log(removeCartButtons)
+  for (var i = 0; i < removeCartButtons.length; i++){
+    var button = removeCartButtons[i]
+    button.addEventListener('click', removeCartItems)
+    }
+  }
